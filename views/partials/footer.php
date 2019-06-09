ddb650ff10063c3c727c74eb9340019b56802059
UlysseGebe <Uly35534e31>
2019-06-10 00:55:53 +0200

    links

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,10 +1,10 @@
     <footer style="position:<?php echo $position?>">
         <div class="links">
-            <img src="assets/image/insta.png" alt="instagram">
-            <img src="assets/image/twitter.png" alt="twitter">
-            <img src="assets/image/Eyem.png" alt="Eyem">
-            <img src="assets/image/dribble.png" alt="dribble">
-            <img src="assets/image/unsplash.svg" alt="unsplash">
+            <img src="assets/image/insta.png" alt="instagram" onclick="location.href='http\:\/\/www.instagram.com/herfrenchness/'">
+            <img src="assets/image/twitter.png" alt="twitter" onclick="location.href='http\:\/\/twitter.com/crosexclarisse'">
+            <img src="assets/image/Eyem.png" alt="Eyem" onclick="location.href='http\:\/\/www.eyeem.com/u/clarissecroset'">
+            <img src="assets/image/dribble.png" alt="dribble" onclick="location.href='http\:\/\/dribbble.com/oaxakar'">
+            <img src="assets/image/unsplash.svg" alt="unsplash" onclick="location.href='http\:\/\/unsplash.com/@herfrenchness'">
         </div>
         <h5>CLARISSE CROSET</h5>
         <h6>2019</h6>
